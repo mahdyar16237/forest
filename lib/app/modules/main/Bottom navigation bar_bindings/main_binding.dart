884ed5bf8c2d7c8b2ea5aed840b5efccc5a9e321
mahdiyar <mahdyar16237@gmail.com>
2025-11-18 16:06:43 +0330

    home done

--- lib/app/modules/main/Bottom navigation bar_bindings/main_binding.dart
+++
@@ -1,9 +0,0 @@
-import 'package:get/get.dart';
-import 'package:forest/app/modules/main/main_controllers/main_controller.dart';
-
-class MainBinding extends Bindings {
-  @override
-  void dependencies() {
-    Get.lazyPut<MainController>(() => MainController());
-  }
-}
